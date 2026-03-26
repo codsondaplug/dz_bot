d984dd8958e222a723d76ec7637c9ec88242c9f9
codsondaplug <avistixbs@gmail.com>
2026-03-26 23:04:34 +0300

    main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+package com.devops.bot;
+
+public class Main {
+
+    public static void greet(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Введите имя");
+        }
+        System.out.println("Привет, " + name + "!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        greet("DevOps");
+        System.out.println("2 + 3 = " + add(2, 3));
+        System.out.println("Работает!");
+    }
+}
